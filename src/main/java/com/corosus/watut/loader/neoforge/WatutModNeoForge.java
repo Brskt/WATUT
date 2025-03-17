f9817aeb68969df78222a9def771df8842b15a11
Corosauce <cordonfreeman@gmail.com>
2025-03-17 20:45:51 +0000

    v1.2.5 update to 1.21.4 Fix resource reloading breaking dynamic gui rendering for fabric Try to send full itemstack for item transferring visual, but if its larger than 31000 bytes, use the simple itemstack version

--- src/main/java/com/corosus/watut/loader/neoforge/WatutModNeoForge.java
+++ src/main/java/com/corosus/watut/loader/neoforge/WatutModNeoForge.java
@@ -1,6 +1,7 @@
 package com.corosus.watut.loader.neoforge;
 
 
+import com.corosus.coroutil.util.MultiLoaderUtil;
 import com.corosus.watut.ShaderRegistry;
 import com.corosus.watut.ShaderReloader;
 import com.corosus.watut.WatutMod;
@@ -41,8 +42,7 @@ public class WatutModNeoForge extends WatutMod {
             NeoForge.EVENT_BUS.addListener(clientEvents::onGameTick);
             NeoForge.EVENT_BUS.addListener(clientEvents::onKey);
             NeoForge.EVENT_BUS.addListener(clientEvents::onMouse);
-
-            ((ReloadableResourceManager)Minecraft.getInstance().getResourceManager()).registerReloadListener(new ShaderReloader());
+            container.getEventBus().addListener(clientEvents::reload);
 
             //container.getEventBus().register(new RegisterShadersEvent());
             /*ShaderRegistry.instance = new ShaderRegistry();
@@ -54,6 +54,8 @@ public class WatutModNeoForge extends WatutMod {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
+
+
 
     }
 
